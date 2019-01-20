a818604057461d60e280c20a7625a8ec097f0046
Nelson Rodrigues <rodrigues.b.nelson@gmail.com>
2019-01-20 21:36:14 +0000

    chapter 07 - diffuse materials

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 #include "camera.h"
 
 #include "sphere.h"
@@ -7,16 +8,25 @@
 
 using namespace std;
 
+vec3 randon_in_unit_sphere(){
+    vec3 p;
+    do {
+        p = 2.0*vec3(drand48(), drand48(), drand48()) - vec3 (1,1,1);
+    } while(dot(p,p) >= 1.0);
+    return p;
+}
+
 vec3 color(const ray& r, hitable *world) {
 
     hit_record rec;
     if(world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5*vec3( rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+        vec3 target = rec.p + rec.normal + randon_in_unit_sphere();
+        return 0.5*color(ray (rec.p, target - rec.p), world);
     }
     else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5*(unit_direction.y() + 1.0);
-         return (1.0 - t )*vec3(1.0, 1.0, 1.0) + t*vec3(0.5,0.7,1.0);
+        return (1.0 - t )*vec3(1.0, 1.0, 1.0) + t*vec3(0.5,0.7,1.0);
      }
 }
 
@@ -38,13 +48,14 @@ int main(int argc, const char * argv[]) {
         for(int i = 0; i < nx; i++){
             vec3 col(0,0,0);
             for( int s = 0; s < ns; s++){
-                float u = float(j) / float (nx);
-                float v = float(j) / float (ny);
+                float u = float(i + drand48()) / float (nx);
+                float v = float(j + drand48()) / float (ny);
                 ray r = cam.get_ray(u,v);
                 vec3 p = r.point_at_parameter(2.0);
                 col += color(r, world);
             }
             col /= float(ns);
+            col = vec3 (sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
             int ir = int (255.99*col[0]);
             int ig = int (255.99*col[1]);
             int ib = int (255.99*col[2]);
