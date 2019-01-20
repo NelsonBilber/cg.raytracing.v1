0f7145b5d36c265b527b7e02f2aa063c97910d37
Nelson Rodrigues <rodrigues.b.nelson@gmail.com>
2019-01-20 19:36:53 +0000

    chapter 5 - hit multiple spheres

--- main.cpp
+++ main.cpp
@@ -1,40 +1,23 @@
 #include <iostream>
 #include "camera.h"
-#include "vec3.h"
-#include "ray.h"
+
+#include "sphere.h"
+#include "hitable_list.h"
+#include "float.h"
 
 using namespace std;
 
+vec3 color(const ray& r, hitable *world){
 
-bool hit_sphere( const vec3& center, float radius, const ray& r){
-
-    vec3 oc = r.orign() - center;
-
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0 * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius*radius;
-    float discrimnant = b*b - 4*a*c;
-
-    if(discrimnant < 0){
-        return -1.0;
+    hit_record rec;
+    if(world->hit(r, 0.0, MAXFLOAT, rec)) {
+        return 0.5*vec3( rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
     }
-    else{
-        return (-b - sqrt(discrimnant)) / (2.0*a);
-    }
-}
-
-vec3 color(const ray& r){
-
-    float t = hit_sphere(vec3(0,0,1),0.5,r);
-    if( t > 0 ){
-            vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
-            return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
-    }
-
-    vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5*(unit_direction.y() + 1.0);
-
-    return (1.0 - t )*vec3(1.0, 1.0, 1.0) + t*vec3(0.5,0.7,1.0);
+    else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5*(unit_direction.y() + 1.0);
+         return (1.0 - t )*vec3(1.0, 1.0, 1.0) + t*vec3(0.5,0.7,1.0);
+     }
 }
 
 int main(int argc, const char * argv[])
@@ -49,6 +32,11 @@ int main(int argc, const char * argv[])
     vec3 vertical(0.0,2.0,0.0);
     vec3 origin(0.0,0.0,0.0);
 
+    hitable *list[2];
+    list[0] = new sphere (vec3(0,0,-1), 0.5);
+    list[1] = new sphere (vec3(0,-100.5,-1), 100);
+    hitable *world = new hitable_list(list, 2);
+
     for(int j = ny-1; j >= 0; j--){
         for(int i = 0; i < nx; i++){
 
@@ -56,7 +44,7 @@ int main(int argc, const char * argv[])
             float v = float(j) / float (ny);
 
             ray r(origin, lower_left_corner + u*horizontal + v*vertical );
-            vec3 col = color(r);
+            vec3 col = color(r, world);
 
             int ir = int (255.99*col[0]);
             int ig = int (255.99*col[1]);
