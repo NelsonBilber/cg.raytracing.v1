4aee7b766a4443c49c4625b167bf294a9d228a9d
Nelson Rodrigues <rodrigues.b.nelson@gmail.com>
2019-01-21 22:26:53 +0000

    chapter 10 - cameras

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ int main(int argc, const char * argv[]) {
     //list[3] = new sphere (vec3(-1,0,-1), 0.5, new metal(vec3(0.8,0.8,0.8)));
 
     hitable *world = new hitable_list(list, 2);
-    camera cam;
+    camera cam(vec3(-2,2,1),vec3(0,0,-1), vec3(0,1,0), 90, float(nx)/float(ny));
 
     for(int j = ny-1; j >= 0; j--){
         for(int i = 0; i < nx; i++){
